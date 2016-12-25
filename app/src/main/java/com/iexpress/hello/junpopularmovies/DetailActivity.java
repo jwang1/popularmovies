8def5fb069252a56143b7e0842f53bb8282b1591
jwang1 <sigkdd@yahoo.com>
2016-12-24 22:27:39 -0500

    Adding "Up" button on ActionBar.
    Triologies :   setting up  in AndroidManifest.xml ;   build it onCreate;   and Response the selection.
    
    1. AndroidManifest.xml  handling parent-child activity hierarchy
        also notice the meta-data tag usage for 4.0 and lower compatibility
    
    2. For child activity, either MovieDetailActivity or MovieSettingActivity,
        when onCreate, set the ActiveBar.
        Note, we used the AppCompactDelegate to getSupportActionBar ,
        not the direct getActionBar() which NOT working for reasons I don't know yet
        ( the getActionBar method returned 'null' ActionBar)
        but the delegate.getSupportActionBar method did return good action bar.
    
    3. Now, on onOptionMenuItemSelect event, the other project, we used
        Setting-Hierarchy (ie, setting-General-header is a actitiy holding sub-settings
        like location, temp-Unit setting, social etc;  those Setting-Fragment's
        onOptionMenuItemSelect will RETURN back to "SettingActivity");
    
        But, this PopularMovie project,  does NOT use "Setting-General-Header" and
        only has one sub-setting (ListPreference of popular or top_rated movies),
        therefore, "Up" button from here will directly Go Back to MainActivity,
        if the selected item is "home",  it will direct take us to the Main menu.

--- app/src/main/java/com/iexpress/hello/junpopularmovies/DetailActivity.java
+++ app/src/main/java/com/iexpress/hello/junpopularmovies/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.iexpress.hello.junpopularmovies;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/iexpress/hello/junpopularmovies/MovieSettingsActivity.java
+++ app/src/main/java/com/iexpress/hello/junpopularmovies/MovieSettingsActivity.java
@@ -1,18 +1,43 @@
 package com.iexpress.hello.junpopularmovies;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.MenuItem;
 
 /**
  * MovieSettingsActivity uses ListPreference fragment.
  */
 public class MovieSettingsActivity extends PreferenceActivity {
 
+  private AppCompatDelegate delegate;
+
+  /**
+   * Handle "Up" button in ActionBar
+   *
+   * @param featureId
+   * @param item
+   * @return
+   */
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    int id = item.getItemId();
+    if (id == android.R.id.home) {
+      if (!super.onMenuItemSelected(featureId, item)) {
+        NavUtils.navigateUpFromSameTask(this);
+      }
+      return true;
+    }
+    return super.onMenuItemSelected(featureId, item);
+  }
 
   private static Preference.OnPreferenceChangeListener preferenceChangeListener = new Preference.OnPreferenceChangeListener() {
     @Override
@@ -49,6 +74,15 @@ public class MovieSettingsActivity extends PreferenceActivity {
     getFragmentManager().beginTransaction()
         .replace(android.R.id.content, new TmdbApiPreferenceFragment())
         .commit();
+
+    // setup Up button
+    if (delegate == null) {
+      delegate = AppCompatDelegate.create(this, null);
+    }
+    ActionBar actionBar = delegate.getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
   }
 
   public static class TmdbApiPreferenceFragment extends PreferenceFragment {
@@ -59,6 +93,16 @@ public class MovieSettingsActivity extends PreferenceActivity {
       setHasOptionsMenu(true);
 
       bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_movie_api_key)));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      int id = item.getItemId();
+      if (id == android.R.id.home) {
+        startActivity(new Intent(getActivity(), MainActivity.class));
+        return true;
+      }
+      return super.onOptionsItemSelected(item);
     }
   }
 
