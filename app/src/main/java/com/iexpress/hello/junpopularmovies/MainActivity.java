f5175813aefc472829cb218553b0294ff44853ae
jwang1 <sigkdd@yahoo.com>
2016-12-27 22:00:06 -0500

    Added AccountAuthenticator, AccountService, ContentProvider, module definition / configuration.

--- app/src/main/java/com/iexpress/hello/junpopularmovies/MainActivity.java
+++ app/src/main/java/com/iexpress/hello/junpopularmovies/MainActivity.java
@@ -1,16 +1,25 @@
 package com.iexpress.hello.junpopularmovies;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import com.facebook.stetho.DumperPluginsProvider;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.dumpapp.DumperPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    final Context context = this;
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
@@ -19,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
           .add(R.id.activity_main, new MovieFragment())
           .commit();
     }
+
+    // Adding Stetho support
+    Stetho.initialize(Stetho.newInitializerBuilder(context)
+      .enableDumpapp(new MyDumperPluginProvider(context))
+      .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(context))
+      .build());
   }
 
   @Override
@@ -40,6 +55,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  private class MyDumperPluginProvider implements DumperPluginsProvider {
+    private Context context;
+
+    public MyDumperPluginProvider(Context context) {
+      this.context = context;
+    }
+
+    @Override
+    public Iterable<DumperPlugin> get() {
+      List<DumperPlugin> plugins = new ArrayList<>();
+
+      for (DumperPlugin defaultPlugin : Stetho.defaultDumperPluginsProvider(context).get()) {
+        plugins.add(defaultPlugin);
+      }
+
+      return plugins;
+    }
   }
 
 }
